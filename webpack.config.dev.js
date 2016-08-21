607371ff067374ddcafc93f522ab7edb2bb505d1
mikatalk <mika.indie@gmail.com>
2016-08-21 12:54:30 -0700

    Add demo page

--- webpack.config.dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('./webpack.config')({
-  isProduction: false,
-  devtool: 'cheap-eval-source-map',
-  port: 50000
-});
--- webpack.config.prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = require('./webpack.config')({
-  isProduction: true,
-  devtool: 'source-map'
-});
